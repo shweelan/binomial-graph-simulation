17de77ef6db7d3604b42657f784efddd47cee2ab
Shweelan Samson <shweelan@gmail.com>
2018-05-24 01:03:53 -0500

    Add centos starup script

---
+++ centos_starter.sh
@@ -0,0 +1,17 @@
+sudo yum -y update
+sudo yum -y install curl
+sudo yum -y install git
+sudo yum -y install java-1.8.0-openjdk-devel java-1.8.0-openjdk
+
+clone_url="https://github.com/shweelan/binomial-graph-simulation.git"
+clone_dir_name="simulation"
+cd "$HOME"
+if [ ! -d "$clone_dir_name" ] ; then
+  git clone "$clone_url" "$clone_dir_name"
+fi
+
+redis_url="http://10.10.0.11:7379"
+
+cd "$clone_dir_name"
+chmod +x "start.sh"
+/bin/bash ./start.sh $redis_url > "start.log"  2>&1 &
